85b8901653f39bf3e1524dc1e8ef346ae0974a1f
Wouter Vanden Hove <wouter.vanden.hove@gmail.com>
2015-01-16 22:15:51 +0100

    add link to authorpage

--- collective/lastlogin/view.py
+++ collective/lastlogin/view.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 from Products.Five import BrowserView
 from Products.CMFCore.utils import getToolByName
+from zope.component import getMultiAdapter 
 
 
 class LastLogin(BrowserView):
 
     def memberinfo(self):
+
+        portal_state = getMultiAdapter((self.context, self.request), name=u'plone_portal_state')
+        portal_url = portal_state.portal_url()
         mtool = getToolByName(self.context, 'portal_membership')
+
         for user in mtool.searchForMembers():
             yield({'id': user.getId(),
                    'login_time': user.getProperty('login_time'),
                    'last_login_time': user.getProperty('last_login_time'),
+                   'author_url': portal_url + '/author/' + user.getId(),
                    })
--- docs/HISTORY.rst
+++ docs/HISTORY.rst
@@ -4,6 +4,9 @@ Changelog
 0.2 (unreleased)
 ----------------
 
+- In the template, add link to the authorpage for each user.
+  [WouterVH]
+
 - Repository moved from SVN Collective to Github.
   Cfr  https://github.com/collective/collective.lastlogin
   [WouterVH]
